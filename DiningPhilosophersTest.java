b3f4d116ed9cf1f6af37c8be67c95d430c8dc29e
ark10806@gmail.com <ark10806@gmail.com>
2021-06-18 23:35:22 +0900

    final

--- DiningPhilosophersTest.java
+++ DiningPhilosophersTest.java
@@ -103,62 +103,74 @@ class DiningPhilosophersTest{
 	public static void main(String[] args){
 		int i, loop;
 		long th0, th1, th2;
-		loop = 1000;
+		th0 = 0;
+		th1 = 0;
+		th2 = 0;
+		loop = 10000;
 		Semaphore[] fork = new Semaphore[num];
-
-		for(i=0; i<num; i++)
-			fork[i] = new Semaphore(1);
-		Semaphore room = new Semaphore(4);
-
-		Philosopher_room[] phil_room = new Philosopher_room[num];
-		for(i=0; i<num; i++)
-			phil_room[i] = new Philosopher_room(i, fork[i], fork[(i+1)%num], room, loop);
-		th0 = System.currentTimeMillis();
-		for(i=0; i<num; i++)
-			phil_room[i].start();
-		try{
+		boolean[] is_on = {false, false, true};
+		
+		if(is_on[0]){
 			for(i=0; i<num; i++)
-				phil_room[i].join();
-		}catch(InterruptedException e){}
-		th0 = System.currentTimeMillis() - th0;
+				fork[i] = new Semaphore(1);
+			Semaphore room = new Semaphore(4);
 
-
-		for(i=0; i<num; i++)
-			fork[i] = new Semaphore(1);
-		Semaphore once = new Semaphore(1);
-
-		Philosopher_once[] phil_once = new Philosopher_once[num];
-		for(i=0; i<num; i++)
-			phil_once[i] = new Philosopher_once(i, fork[i], fork[(i+1)%num], once, loop);
-		th1 = System.currentTimeMillis();
-		for(i=0; i<num; i++)
-			phil_once[i].start();
-		try{
+			Philosopher_room[] phil_room = new Philosopher_room[num];
 			for(i=0; i<num; i++)
-				phil_once[i].join();
-		}catch(InterruptedException e){}
-
-		th1 = System.currentTimeMillis() - th1;
-
-
-		for(i=0; i<num; i++)
-			fork[i] = new Semaphore(1);
-		Philosopher_acyclic[] phil_ac = new Philosopher_acyclic[num];
-		for(i=0; i<num; i++)
-			phil_ac[i] = new Philosopher_acyclic(i, fork[i], fork[(i+1)%num], loop);
-
-		th2 = System.currentTimeMillis();
-		for(i=0; i<num; i++)
-			phil_ac[i].start();
-		try{
+				phil_room[i] = new Philosopher_room(i, fork[i], fork[(i+1)%num], room, loop);
+			th0 = System.currentTimeMillis();
 			for(i=0; i<num; i++)
-				phil_ac[i].join();
-		}catch(InterruptedException e){}
+				phil_room[i].start();
+			try{
+				for(i=0; i<num; i++)
+					phil_room[i].join();
+			}catch(InterruptedException e){}
+			th0 = System.currentTimeMillis() - th0;
+		}
 
-		th2 = System.currentTimeMillis() - th2;
+		if(is_on[1]){
+			for(i=0; i<num; i++)
+				fork[i] = new Semaphore(1);
+			Semaphore once = new Semaphore(1);
 
-		System.out.println("room:" + th0 + "ms");
-		System.out.println("once:" + th1 + "ms");
-		System.out.println("acyc:" + th2 + "ms");
+			Philosopher_once[] phil_once = new Philosopher_once[num];
+			for(i=0; i<num; i++)
+				phil_once[i] = new Philosopher_once(i, fork[i], fork[(i+1)%num], once, loop);
+			th1 = System.currentTimeMillis();
+			for(i=0; i<num; i++)
+				phil_once[i].start();
+			try{
+				for(i=0; i<num; i++)
+					phil_once[i].join();
+			}catch(InterruptedException e){}
+
+			th1 = System.currentTimeMillis() - th1;
+		}
+
+		
+		if(is_on[2]){
+			for(i=0; i<num; i++)
+				fork[i] = new Semaphore(1);
+			Philosopher_acyclic[] phil_ac = new Philosopher_acyclic[num];
+			for(i=0; i<num; i++)
+				phil_ac[i] = new Philosopher_acyclic(i, fork[i], fork[(i+1)%num], loop);
+
+			th2 = System.currentTimeMillis();
+			for(i=0; i<num; i++)
+				phil_ac[i].start();
+			try{
+				for(i=0; i<num; i++)
+					phil_ac[i].join();
+			}catch(InterruptedException e){}
+
+			th2 = System.currentTimeMillis() - th2;
+		}
+
+		if(is_on[0])
+			System.out.println("room:" + th0 + "ms");
+		if(is_on[1])
+			System.out.println("once:" + th1 + "ms");
+		if(is_on[2])
+			System.out.println("acyc:" + th2 + "ms");
 	}
 }
